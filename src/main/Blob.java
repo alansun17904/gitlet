ab2b958df9564e8ef518df26569aeb623cea5938
alansun17904 <asun17904@gmail.com>
2020-08-22 22:41:44 -0400

    implemented all unit tests for SHA-1 hash generation, comparison between blobs; implemented constructor for Blob class

--- src/main/Blob.java
+++ src/main/Blob.java
@@ -16,15 +16,21 @@ public class Blob implements Serializable {
 	private String content;
 	
 	public Blob(String filename) {
-		this.filename = filename;
+		this.filename = System.getProperty("user.dir") + filename;
+		this.content = "";
 		try {
 			File blob = new File(this.filename);
 			Scanner reader = new Scanner(blob);
 			while (reader.hasNextLine()) {
-				return;
+				this.content = this.content + reader.nextLine();
+				if (reader.hasNextLine()) {
+					this.content = this.content + "\n";
+				}
 			}
+			System.out.println(this.content);
+			reader.close();
 		} catch (FileNotFoundException e) {
-			return;
+			System.out.println("File not found.");
 		}
 	}
 	
@@ -34,5 +40,9 @@ public class Blob implements Serializable {
 	
 	public static String hash(String hashText) {
 		return "";
+	}
+	
+	public boolean equals(Blob other) {
+		return this.hash.equals(other.hash);
 	}
 }
--- src/tests/TestBlob.java
+++ src/tests/TestBlob.java
@@ -7,20 +7,45 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestBlob {
+	private main.Blob blob1;
+	private main.Blob blob2;
+	private main.Blob blob3;
 	
 	@Before
 	public void before() throws Exception {
-		main.Blob blob = new main.Blob("../tests/introduction.txt");
+		this.blob1 = new main.Blob("/src/tests/resources/introduction1.txt");
+		this.blob2 = new main.Blob("/src/tests/resources/introduction2.txt");
+		this.blob3 = new main.Blob("/src/tests/resources/random.txt");
 	}
 	
 	@Test
 	public void testSHA1() {
 		assertNotEquals(main.Blob.hash("hello world"), 
 				   		main.Blob.hash("my name is alan"));
+	}
+	
+	@Test
+	public void testSHA2() {
 		assertNotEquals(main.Blob.hash("   sdf.txt"), 
 						main.Blob.hash("sdf.txt"));
+	}
+	
+	@Test 
+	public void testSHA3() {
 		assertEquals("0190E761BBA7BF93FAC099718DDB33FD9B3BEA1F",
 						main.Blob.hash("hello world."));
+	}
+	
+	@Test 
+	public void testCompareToEqual() {
+		assertTrue(this.blob1.equals(blob2));
+		assertTrue(this.blob2.equals(blob1));
+	}
+	
+	@Test
+	public void testCompareToNotEqual() {
+		assertTrue(!this.blob1.equals(blob3));
+		assertTrue(!this.blob2.equals(blob3));
 	}
 	
 	@After 
